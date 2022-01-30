bd158aa892516effc5a8a08c289284ccb8ee3a13
Kiril Vasilita <everest0corp@gmail.com>
2022-01-31 01:29:57 +0200

    updated pipeline file

--- main.tf
+++ main.tf
@@ -5,31 +5,40 @@ provider "azurerm" {
   }
 }
 
+terraform {
+  backend "azurerm" {
+      resource_group_name  = "tf_rg_blobstorage"
+      storage_account_name = "tfstorageaccountvkiril"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "tf_test" {
   name     = "tfmainrg"
   location = "West Europe"
 }
 
-# resource "azurerm_container_group" "tfcg_test" {
-#   name                      = "weatherapi"
-#   location                  = azurerm_resource_group.tf_test.location
-#   resource_group_name       = azurerm_resource_group.tf_test.name
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
 
-#   ip_address_type           = "public"
-#   dns_name_label            = "vkiril"
-#   os_type                   = "Linux"
+  ip_address_type           = "public"
+  dns_name_label            = "vkirilwa"
+  os_type                   = "Linux"
 
-#   container {
-#     name    = "weatherapi"
-#     image   = "vkiril/weatherapi"
-#     cpu     = "1"
-#     memory  = "1"
+  container {
+    name    = "weatherapi"
+    image   = "vkiril/weatherapi"
+      cpu     = "1"
+      memory  = "1"
 
-#     ports {
-#       port          = 80
-#       protocol      = "TCP"
-#     }
-#   }
-# }
+      ports {
+        port          = 80
+        protocol      = "TCP"
+      }
+  }
+}
 
 
---
+++ readme.md
@@ -0,0 +1,7 @@
+1. terraform init - to init all the stuff
+
+2. terraform plan - to see what;s the plan and costs and stuff
+
+3. az login in case it's needed 
+
+ 
